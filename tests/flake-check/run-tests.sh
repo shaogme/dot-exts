0a4198b07edb3a964f3163f39f58bb45ca9de594
韶光 <87350612+shaogme@users.noreply.github.com>
2026-01-27 21:59:47 +0800

    Enhance run-tests.sh to allow dirty builds during Flake integration checks

--- tests/flake-check/run-tests.sh
+++ tests/flake-check/run-tests.sh
@@ -14,8 +14,10 @@ echo "Checking Flake integration..."
 # 3. Kernel and Btrfs configurations are valid and don't conflict
 # 4. Top-level derivation can be instantiated
 
-nix build .#nixosConfigurations.testMachine.config.system.build.toplevel --dry-run --show-trace --extra-experimental-features flakes  \
+nix build .#nixosConfigurations.testMachine.config.system.build.toplevel --dry-run --show-trace \
+  --extra-experimental-features flakes \
   --option substituters "$CACHE_Substituters" \
-  --option trusted-public-keys "$CACHE_TrustedPublicKeys"
+  --option trusted-public-keys "$CACHE_TrustedPublicKeys" \
+  --allow-dirty
 
 echo "Flake integration check passed: Top-level derivation is valid."
